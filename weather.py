815d986dfa0e2e0434488e9ac2860911498317cc
Gabriel Santos <biel2013as@gmail.com>
2023-09-15 19:57:48 -0300

    Add a windw
    Add a window to show the informations

--- weather.py
+++ weather.py
@@ -1,13 +1,31 @@
 import requests
+from tkinter import *
 
-apiKey = "1091a6ac7fd857c4848b84c781f0a52e"
+apiKey = "cb24804aca9b3a19690244edc902e4a8"
+city = ""
 
-city = input("Coloque uma cidade: ")
+def clima():
+    global city
+    city = entry.get()
+    link = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&lang=pt_br"
+    requisicao = requests.get(link)
+    requisicaoDic = requisicao.json()
+    descricao = requisicaoDic['weather'][0]['description']
+    temperatura = requisicaoDic['main']['temp'] - 273.15
+    texto = descricao, f'{round(temperatura, 1)}'"°C"
+    L2["text"] = texto
+    print(descricao, f'{round(temperatura, 1)}'"°C")
 
-link = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&lang=pt_br"
+janela = Tk()
+janela.title("Clima")
+janela.geometry("250x400")
+L1 = Label(janela, text="Coloque uma cidade", font=40)
+L1.grid(row=0, column=1, padx=15)
+L2 = Label(janela, text="", font=40)
+L2.grid(row=3, column=1, padx=15, pady=20)
+entry = Entry(janela)
+entry.grid(row=1, column=1, padx= 15, pady=20)
 
-requisicao = requests.get(link)
-requisicaoDic = requisicao.json()
-descricao = requisicaoDic['weather'][0]['description']
-temperatura = requisicaoDic['main']['temp'] - 273.15
-print(descricao ,f'{temperatura}'"°C")
+botao = Button(janela, text="Pesquisar", command=clima, font=40)
+botao.grid(row=2, column=1)
+janela.mainloop()
